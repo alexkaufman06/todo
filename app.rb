7e800834ee85c484905f8d05e446694f1a60a8fb
Alex Kaufman & David Zulaica <student@epicodus.com>
2015-01-13 10:18:36 -0800

    Update erb files, app.rb.

---
+++ app.rb
@@ -0,0 +1,16 @@
+require("sinatra")
+require("sinatra/reloader")
+also_reload("lib/**/*.rb")
+require("./lib/todo")
+
+get("/") do
+  @todo = Todo.all()
+  erb(:index)
+end
+
+post("/todo") do
+  description = params.fetch("description")
+  todo = Todo.new(description)
+  todo.save()
+  erb(:success)
+end
